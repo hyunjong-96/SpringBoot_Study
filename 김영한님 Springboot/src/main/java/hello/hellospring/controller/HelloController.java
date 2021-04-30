11931855c5ef98edccbb252b32f8eb2cf9c7f405
hyunjong_96 <ahajongs@gmail.com>
2021-05-01 00:40:18 +0900

    #김영한님 SpringBoot 프로젝트 환경설정

--- 김영한님 Springboot/src/main/java/hello/hellospring/controller/HelloController.java
+++ 김영한님 Springboot/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,6 +1,7 @@
 package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
