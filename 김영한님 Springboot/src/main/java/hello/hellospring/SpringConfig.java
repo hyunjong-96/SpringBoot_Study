b24a230900af2ffaf2b63eb076d9e316080cba7d
hyunjong_96 <ahajongs@gmail.com>
2021-05-04 15:48:52 +0900

    #김영한님 springboot_스프링 스프링 데이터 JPA

--- 김영한님 Springboot/src/main/java/hello/hellospring/SpringConfig.java
+++ 김영한님 Springboot/src/main/java/hello/hellospring/SpringConfig.java
@@ -2,31 +2,46 @@ package hello.hellospring;
 
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(DataSource dataSource){
-        this.dataSource = dataSource;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+//    private EntityManager em;
+//
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+//    private DataSource dataSource;
+//
+//    public SpringConfig(DataSource dataSource){
+//        this.dataSource = dataSource;
+//    }
 
     @Bean
     public MemberService testOfService(){
-        return new MemberService(testRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository testRepository(){
-        return new JdbcTemplateMemberRepository(dataSource);
-        //return new JdbcMemberRepository(dataSource);
-        //return new MemoryTestRepository();
-    }
+//    @Bean
+//    public MemberRepository testRepository(){
+//        return new JpaMemberRepository(em);
+//        //return new JdbcTemplateMemberRepository(dataSource);
+//        //return new JdbcMemberRepository(dataSource);
+//        //return new MemoryTestRepository();
+//    }
 }
--- 김영한님 Springboot/src/main/java/hello/hellospring/service/MemberService.java
+++ 김영한님 Springboot/src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Primary
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
